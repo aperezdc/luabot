f149a39d81060af1a0fa352e81f36d432d934a15
Adrián Pérez de Castro <aperez@igalia.com>
2015-09-26 18:39:02 +0200

    invite: Use nick name from configuration file
    This patch makes the "invite" plugin use the nick name from the configuration:
    either the one from the plugin configuration, or if not given, the globally
    configured nick name.

--- plugin/invite.lua
+++ plugin/invite.lua
@@ -10,7 +10,9 @@ local xmlns_muc = "http://jabber.org/protocol/muc"
 local xmlns_muc_user = xmlns_muc .. "#user"
 local xmlns_jxc = "jabber:x:conference"
 
-return function (bot)
+return function (bot, invite_cfg, bot_cfg)
+	local default_nick = invite_cfg.nick or bot_cfg.nick
+
 	bot:hook("message", function (event)
 		local x = event.stanza:get_child("x", xmlns_muc_user)
 
@@ -18,14 +20,12 @@ return function (bot)
 		if x then
 			local invite = x:get_child("invite")
 			if invite then
-				-- FIXME: Do not hardcode nickname
-				bot:join_room(event.stanza.attr.from, "luabot")
+				bot:join_room(event.stanza.attr.from, default_nick)
 			end
 		else
 			x = event.stanza:get_child("x", xmlns_jxc)
 			if x and x.attr.jid then
-				-- FIXME: Do not hardcode nickname
-				bot:join_room(x.attr.jid, "luabot")
+				bot:join_room(x.attr.jid, default_nick)
 			end
 		end
 	end)
