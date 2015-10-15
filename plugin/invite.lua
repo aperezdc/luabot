d4807775abd80a22e650997c93457fcac1e5bcc5
Adrián Pérez de Castro <aperez@igalia.com>
2015-10-15 19:14:41 +0300

    invite: Fix accesses to unavailable event.bot attribute
    Events don't have an event.bot attribute, so the "bot" variable has to be
    captured using lexical scoping.

--- plugin/invite.lua
+++ plugin/invite.lua
@@ -11,26 +11,23 @@ local xmlns_muc_user = xmlns_muc .. "#user"
 local xmlns_jxc = "jabber:x:conference"
 
 
-local function handle_message(event)
-	local x = event.stanza:get_child("x", xmlns_muc_user)
-
-	-- Try both XEP-45 and XEP-249
-	if x then
-		local invite = x:get_child("invite")
-		if invite then
-			local nick = bot.config.plugin.invite.nick or bot.config.nick
-			event.bot:join_room(event.stanza.attr.from, nick)
-		end
-	else
-		x = event.stanza:get_child("x", xmlns_jxc)
-		if x and x.attr.jid then
-			local nick = bot.config.plugin.invite.nick or bot.config.nick
-			event.bot:join_room(x.attr.jid, nick)
-		end
-	end
-end
-
-
 return function (bot)
-	bot:hook("message", handle_message)
+	bot:hook("message", function (event)
+		local x = event.stanza:get_child("x", xmlns_muc_user)
+
+		-- Try both XEP-45 and XEP-249
+		if x then
+			local invite = x:get_child("invite")
+			if invite then
+				local nick = bot.config.plugin.invite.nick or bot.config.nick
+				bot:join_room(event.stanza.attr.from, nick)
+			end
+		else
+			x = event.stanza:get_child("x", xmlns_jxc)
+			if x and x.attr.jid then
+				local nick = bot.config.plugin.invite.nick or bot.config.nick
+				bot:join_room(x.attr.jid, nick)
+			end
+		end
+	end)
 end
