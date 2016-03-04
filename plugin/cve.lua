6cf002fd1507c8a185a137662b9eb2090ea9f033
Adrián Pérez de Castro <aperez@igalia.com>
2016-03-04 23:47:38 +0200

    cve: Use the JSON-based API from cve.circl.lu
    Using actual JSON data is much nicer than extracting the title of an HTML
    document.

--- plugin/cve.lua
+++ plugin/cve.lua
@@ -6,22 +6,24 @@
 -- Distributed under terms of the MIT license.
 --
 
-local html_title = require("util.html").extract_title
 local urlfetch = require("util.urlfetch")
+local json = require("util.json")
 
-local base_url = "https://www.cvedetails.com/cve/CVE-%s"
+local base_url = "https://cve.circl.lu/cve/CVE-%s"
+local api_url = "https://cve.circl.lu/api/cve/CVE-%s"
 
 local function handle_cve(shortcut)
-	local url = base_url:format(shortcut.param)
+	local url = api_url:format(shortcut.param)
 	urlfetch(url, nil, function (data, code, _, url)
-		if code ~= 200 then return end
+		if code ~= 200 or not data or #data == 0 then return end
 
-		local title = html_title(data)
-		if title then
-			shortcut:post(url .. " - " .. title .. "…")
-		else
-			shortcut:post(url)
-		end
+		data = json.decode(data)
+		if not data then return end
+
+		local score = data.cvss and " (Score: " .. data.cvss .. ")" or " (no score)"
+		shortcut:post("CVE " .. shortcut.param .. ": " ..
+                    data.summary .. score)
+		shortcut:post(base_url:format(shortcut.param))
 	end)
 	return true
 end
