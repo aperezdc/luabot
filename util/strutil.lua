2eca5f053b80a6143f1b1e549950e69208788c23
Adrian Perez de Castro <aperez@igalia.com>
2016-05-16 23:29:25 +0300

    Make strutil.interpolate resolve dotted paths
    This makes strutil.interpolate() support "dotted paths" in variable
    expansions. This allows template strings to obtain values from nested
    tables:
    
        strutil.interpolate("%{foo.bar}", { foo = { bar = 42 } })
    
    evaluates to "42". If any of the intermediate values resolved to "nil",
    or a value cannot be indexed (e.g. is "foo" above wouldn't be a table)
    then the expansion is not performed:
    
        strutil.interpolate("%{foo} %{foo.bar.baz}", { foo = true })
    
    evaluates to "true %{foo.bar.baz}" because the dotted path in the second
    expansion cannot be resolved.

--- util/strutil.lua
+++ util/strutil.lua
@@ -6,28 +6,51 @@
 -- Distributed under terms of the MIT license.
 --
 
-local s_match = string.match
-local s_gsub  = string.gsub
+local s_gmatch    = string.gmatch
+local s_match     = string.match
+local s_gsub      = string.gsub
+local s_sub       = string.sub
+local _tostring   = tostring
+local _require    = require
+local _type       = type
+
+-- Lazy-load "util.html" to avoid mutually-recursive calls to require()
+local html_escape = function (s)
+   html_escape = _require("util.html").escape
+   return html_escape(s)
+end
 
 --
 -- Based on Rici Lake's simple string interpolation
 -- See: http://lua-users.org/wiki/StringInterpolation
 --
-local function interpolate(text, vars)
-	local html_escape = require("util.html").escape
-	return (text:gsub('([$%%]%b{})', function (w)
-		local value = vars[w:sub(3, -2)]
-		if value ~= nil then
-			value = tostring(value)
-			if w:sub(1, 1) == "$" then
-				return html_escape(value)
-			else
-				return value
-			end
-		else
-			return w
-		end
-	end))
+local dotted_path_pattern = "[^%.]+"
+local function interpolate(text, value)
+   return (s_gsub(text, "([$%%]%b{})", function (dotted_path)
+      local completed = true
+      for component in s_gmatch(s_sub(dotted_path, 3, -2), dotted_path_pattern) do
+         if _type(value) ~= "table" then
+            completed = false
+            break
+         end
+         if _type(value[component]) == "function" then
+            -- Pass the current value as "self"
+            value = value[component](value)
+         else
+            value = value[component]
+         end
+      end
+      if completed then
+         value = _tostring(value)
+         if s_sub(dotted_path, 1, 1) == "$" then
+            return html_escape(value)
+         else
+            return value
+         end
+      else
+         return dotted_path
+      end
+   end))
 end
 
 
